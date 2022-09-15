37c3f2144ce03b238f76e1b56d6b8e61a294c845
Adam Pecan <adamnyk@gmail.com>
2022-09-14 18:21:06 -0600

    added rest/spread

---
+++ 15.4_rest-spread.js
@@ -0,0 +1,100 @@
+// Given this function:
+// function filterOutOdds() {
+//   var nums = Array.prototype.slice.call(arguments);
+//   return nums.filter(function(num) {
+//     return num % 2 === 0
+//   });
+// }
+// Refactor it to use the rest operator & an arrow function:
+
+const filterEvens = (...nums) => nums.filter((num)=> num % 2 === 0);
+
+
+// findMin
+// Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
+
+// Make sure to do this using the rest and spread operator.
+
+// findMin(1,4,12,-3) // -3
+// findMin(1,-1) // -1
+// findMin(3,1) // 1
+
+const findMin = (...nums) => nums.reduce((min, eachNum) => eachNum < min ? eachNum : min);
+
+
+
+// mergeObjects
+// Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+// mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2});
+
+
+// doubleAndReturnArgs
+// Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+
+// doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
+// doubleAndReturnArgs([2],10,4) // [2, 20, 8]
+
+const doubleAndReturnArgs = (arr, ...rest) => [...arr, ...rest.map((num) => num * 2)];
+
+
+// Slice and Dice!
+// For this section, write the following functions using rest, spread and refactor these functions to be arrow functions!
+
+// Make sure that you are always returning a new array or object and not modifying the existing inputs.
+
+// /** remove a random element in the items array
+// and return a new array without that item. */
+
+// using .splice
+const removeRandom = (items) => {
+  const randomIdx = Math.floor(Math.random() * items.length)
+  const smallerArr = items;
+  smallerArr.splice(randomIdx, 1);
+  return smallerArr;
+};
+
+// Using .slice
+const removeRandom2 = (items) => {
+  const randomIdx = Math.floor(Math.random() * items.length)
+  return [...items.slice(0, randomIdx), ...items.slice(randomIdx+1)]
+};
+
+
+
+// /** Return a new array with every item in array1 and array2. */
+
+const extend = (array1, array2) => [...array1, ...array2]
+
+
+
+// /** Return a new object with all the keys and values
+// from obj and a new key/value pair */
+
+const addKeyVal = (obj, key, val) => {
+  const newObj = {...obj};
+  newObj[key] = val;
+  return newObj;
+};
+
+
+// /** Return a new object with a key removed. */
+
+const removeKey = (obj, key) => {
+  newObj = {...obj};
+  delete newObj[key];
+  return newObj;
+}
+
+const testObj = {a:1, b:2, c:3}
+const testObj2 = {d:4, e:5, f:6}
+
+// /** Combine two objects and return a new object. */
+
+const combine = (obj1, obj2) => ({...obj1, ...obj2})
+
+
+// /** Return a new object with a modified key and value. */
+
+const update = (obj, key, val) => ({...obj, [key]: val})
